8a4e7f6dec4bcb9dceeb4b86d81cea1abbaeafa6
GendessonSousa <gendessonsousa078@gmail.com>
2025-08-28 19:24:06 -0300

    feat: Coloca o Lombok nas classes para abstração

--- CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.Gendesson.CadastroDeNinjas.Ninjas;
 
 import dev.Gendesson.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -15,41 +21,10 @@ public class NinjaModel {
      private String nome;
      private String email;
      private int idade;
+
      @ManyToOne
      @JoinColumn(name = "missoes_id") //Foreign key
      private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
 }
