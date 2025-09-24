01131bb1533525920989676fdf658e9ff46205ef
GendessonSousa <gendessonsousa078@gmail.com>
2025-09-24 13:23:21 -0300

    Altera a rota do controller para listar os ninjas e injeta a dependência do service e do repository

--- CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,9 +2,16 @@ package dev.Gendesson.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
@@ -19,8 +26,8 @@ public class NinjaController {
 
     //Mostrar todos os ninjas (READ)
     @GetMapping("/listar")
-    public String mostrarTodos(){
-        return "Mostrar todos";
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
     }
 
     //Mostrar ninja por ID (READ)
--- CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,23 @@
 package dev.Gendesson.CadastroDeNinjas.Ninjas;
 
+import jdk.dynalink.linker.LinkerServices;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Listar ninjas
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+
+    }
+
+
 }
