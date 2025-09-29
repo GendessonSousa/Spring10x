eedccbb9247cbc7f0fb1bc2806bfd2500d21e9a5
GendessonSousa <gendessonsousa078@gmail.com>
2025-09-29 00:25:26 -0300

    fix: inclui a coluna 'rank' na entidade, porém já estava incluida na migration V2

--- CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -27,6 +27,8 @@ public class NinjaModel {
      private int idade;
      @Column(name = "img_url")
      private String imgUrl;
+     @Column(name = "rank")
+     private String rank;
 
      @ManyToOne
      @JoinColumn(name = "missoes_id") //Foreign key
@@ -35,7 +37,56 @@ public class NinjaModel {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
     }
 }
