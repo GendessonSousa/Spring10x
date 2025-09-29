f4b46f09055915780ca5b5244343fd721005d817
GendessonSousa <gendessonsousa078@gmail.com>
2025-09-29 00:28:52 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no mapper os caminhos entidade x DTO

--- CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ CadastroDeNinjas/src/main/java/dev/Gendesson/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,75 @@
 package dev.Gendesson.CadastroDeNinjas.Ninjas;
 
+import dev.Gendesson.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private MissoesModel missoes;
+    private String rank;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
 }
